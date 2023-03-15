b83f68cf3a970740dc26569f03a6d7501f419392
BisiDavi <oludavidconnect@gmail.com>
2023-03-15 05:17:16 +0100

    our-leadership added

---
+++ template-parts/about-us/our-leadership.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package Wordpress
+ * @subpackage Our Leadership
+ * @since 1.0
+ */
+
+?>
+
+<div class="our-leadership">
+    <div class="container mx-lg-5 py-5 py-lg-0 m-3">
+        <? $postArgs = array(
+            'post_type' => 'post', 
+            'cat' => 13, 
+        );
+            $leadershipPost = new WP_Query($postArgs);
+            if ($leadershipPost->have_posts()) {
+                while ($leadershipPost->have_posts()) {
+                    $leadershipPost->the_post();
+                    get_template_part('template-parts/home/hero-slider', 'hero-slider');
+                }
+            } 
+        ?>
+    </div>
+</div>
