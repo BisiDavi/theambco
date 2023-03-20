97a0bce92ac771de7e24b172972ae9a82b599d35
BisiDavi <oludavidconnect@gmail.com>
2023-03-20 14:35:47 +0100

    pages updated

---
+++ page-services.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @package Wordpress
+ * @subpackage TheAmbulanceCompany
+ * @since TheAmbulanceCompany 1.0
+ */
+
+get_header('home');
+
+
+// load slider post loop
+get_template_part('template-parts/home/hero-banner', 'hero-banner');
+
+
+get_template_part('template-parts/home/contact-us-view', 'contact-us-view');
+
+get_footer('home');
+
+?>
