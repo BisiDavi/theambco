dff271543fc3eb414a174805e367607668f62b84
BisiDavi <oludavidconnect@gmail.com>
2023-03-20 13:05:15 +0100

    medium-post_item page updated

--- template-parts/blog/medium-post_item.php
+++ template-parts/blog/medium-post_item.php
@@ -1,6 +1,9 @@
 <? $post = $args['post']; ?>
+<? $date = date_create($post->pubDate); ?>
+<? $publishedOn = date_format($date,"l, F Y"); ?>
 
 <div class="post-view">
     <img src="<? echo $post->thumbnail; ?>" alt="<? echo $post->title; ?>" />
     <h6> <? echo $post->title; ?> </h6>
+    <p>Posted on: <? echo $publishedOn; ?> </p>
 </div>
