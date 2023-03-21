c93a8bf5559b1c0bdd80eb947005805f5f5d7ecf
BisiDavi <oludavidconnect@gmail.com>
2023-03-21 13:33:12 +0100

    ambulance added

--- template-parts/top-banner.php
+++ template-parts/top-banner.php
@@ -1,12 +1,13 @@
 <? $post_title = get_post_field('post_title',133);?>
 
-<div class="top-banner d-flex align-item-center">
+<div class="top-banner d-flex">
     <div class="container emergency_line d-flex">
-        <div class="text-center">
+        <div class="text-center d-flex align-items-center gap-4">
+            <img src="<? echo get_template_directory_uri(); ?>/assets/images/ambulance.svg" width="20px" height="20px"  class="mx-2" alt="ambulance" />
             <? echo $post_title; ?>
-            <a href="tel:<? echo the_field('phone_number_1',133); ?>"><? echo the_field('phone_number_1',133); ?></a>
-            <a href="tel:<? echo the_field('phone_number_2',133); ?>"><? echo the_field('phone_number_2',133); ?></a>
-            <a href="tel:<? echo the_field('phone_number_3',133); ?>"><? echo the_field('phone_number_3',133); ?></a>
+            <a href="tel:<? echo the_field('phone_number_1',133); ?>" class="mx-1 ml-3"><? echo the_field('phone_number_1',133); ?></a>,
+            <a href="tel:<? echo the_field('phone_number_2',133); ?>" class="mx-1"><? echo the_field('phone_number_2',133); ?></a>,
+            <a href="tel:<? echo the_field('phone_number_3',133); ?>" class="mx-1"><? echo the_field('phone_number_3',133); ?></a>
         </div>
     </div>
 </div>
