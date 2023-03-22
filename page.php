6654e778816f8fc74672520449c709bb778950ad
BisiDavi <oludavidconnect@gmail.com>
2023-03-22 20:32:42 +0100

    pages updated

--- page.php
+++ page.php
@@ -10,7 +10,7 @@
 get_header('home');
 
 // load slider post loop
-get_template_part('template-parts/blog/blog-page', 'blog-page');
+get_template_part('template-parts/blog/blog-view', 'blog-view');
 
 get_footer('home');
 ?>
