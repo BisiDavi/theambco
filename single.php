c9853d6d14eb7cd9a924d1e3ee7270fd86ace9e7
BisiDavi <oludavidconnect@gmail.com>
2023-03-23 15:29:44 +0100

    page updated

--- single.php
+++ single.php
@@ -9,19 +9,6 @@
 
     get_header('home');
 
-    function debug_to_console($data, $context = 'Debug in Console') {
-
-        // Buffering to solve problems frameworks, like header() in this and not a solid return.
-        ob_start();
-    
-        $output  = 'console.info(\'' . $context . ':\');';
-        $output .= 'console.log(' . json_encode($data) . ');';
-        $output  = sprintf('<script>%s</script>', $output);
-    
-        echo $output;
-    }
-
-
     global $wp;
     $page_url = home_url($wp->request);
     $page_slug = explode('/',$page_url)[3];
