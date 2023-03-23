db3d8af0f51996b56c88bb434b7eb474b9fcfe68
BisiDavi <oludavidconnect@gmail.com>
2023-03-23 14:03:19 +0100

    page updated

--- page.php
+++ page.php
@@ -1,17 +1,3 @@
-<?
-function debug_to_console($data, $context = 'Debug in Console') {
-
-    // Buffering to solve problems frameworks, like header() in this and not a solid return.
-    ob_start();
-
-    $output  = 'console.info(\'' . $context . ':\');';
-    $output .= 'console.log(' . json_encode($data) . ');';
-    $output  = sprintf('<script>%s</script>', $output);
-
-    echo $output;
-}
-?>
-
 <?php
 
 /**
@@ -28,19 +14,15 @@ function debug_to_console($data, $context = 'Debug in Console') {
     $page_url = home_url($wp->request);
     $page_slug = explode('/blog/',$page_url)[1];
 
-    debug_to_console($page_slug,'page_slug');
-
     $postArgs = array(
         'post_type' => 'page',
         'name' => $page_slug
     );
 
     $blogPost = new WP_Query($postArgs);
-    debug_to_console($blogPost,'blogPost');
     if($blogPost->have_posts()){
         while($blogPost->have_posts()){
             $blogPost->the_post();
-            debug_to_console($blogPost,'blogPost');
             get_template_part('template-parts/blog/blog-view', 'blog-view');
         }
     }
