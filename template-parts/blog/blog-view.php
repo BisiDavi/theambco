690df6b401347fa1442ea11e00969383c1ba6fed
BisiDavi <oludavidconnect@gmail.com>
2023-03-23 12:55:10 +0100

    blog-view updated

--- template-parts/blog/blog-view.php
+++ template-parts/blog/blog-view.php
@@ -1,6 +1,6 @@
 <div class="container d-flex py-5 blog_view_page">
     <div class="col-12">
-        <h2 class="text-center mb-4 font-bold"> <? echo the_title();  ?> </h2>
+        <h2 class="text-center font-bold"> <? echo the_title();  ?> </h2>
         <div class="blog_image_view">
             <? echo the_post_thumbnail(); ?>
         </div>
