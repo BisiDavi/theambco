4ee8181ce6fb57ce1f6af8fd60c1508a466fe6ea
BisiDavi <oludavidconnect@gmail.com>
2023-03-24 13:29:28 +0100

    post-tag added

--- template-parts/blog/blog-view.php
+++ template-parts/blog/blog-view.php
@@ -1,11 +1,41 @@
-<div class="container d-flex py-5 blog_view_page">
-    <div class="col-12">
-        <h2 class="text-center font-bold"> <? echo the_title();  ?> </h2>
-        <div class="blog_image_view">
-            <? echo the_post_thumbnail(); ?>
+<? 
+function debug_to_console($data, $context = 'Debug in Console') {
+
+    // Buffering to solve problems frameworks, like header() in this and not a solid return.
+    ob_start();
+
+    $output  = 'console.info(\'' . $context . ':\');';
+    $output .= 'console.log(' . json_encode($data) . ');';
+    $output  = sprintf('<script>%s</script>', $output);
+
+    echo $output;
+}
+$post_tags = get_the_tags($post->ID);
+debug_to_console($post_tags, 'post_tag');
+?>
+
+<div class="p-5 blog_view_page">
+    <h2 class="text-center font-bold"> <? echo the_title();  ?> </h2>
+    <div class="container-fluid d-flex mx-auto">
+        <div class="col-9">
+            <div class="blog_image_view">
+                <? echo the_post_thumbnail(); ?>
+            </div>
+            <div class="blog_text-content my-4">
+                <? echo the_content(); ?>
+            </div>
         </div>
-        <div class="blog_text-content my-4">
-            <? echo the_content(); ?>
+        <div class="col-3">
+            <div class="tags gap-4">
+                <h6>Post Tags</h6>
+                <div class="post_tags align-items-center justify-content-between  d-flex">
+                    <? 
+                        foreach ($post_tags as $post_tag) { 
+                            get_template_part('template-parts/blog/post-tag', 'post-tag', array('post_tag' => $post_tag));
+                        }
+                    ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
---
+++ template-parts/blog/post-tag.php
@@ -0,0 +1,5 @@
+<? $post_tag = $args['post_tag'] ?>
+
+<div class="post-tag">
+    <? echo $post_tag->name; ?>
+</div>
