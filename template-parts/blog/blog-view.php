db48004bf7ccf366801ccd768fca31b3a53769af
BisiDavi <oludavidconnect@gmail.com>
2023-03-24 14:54:08 +0100

    blog post updated

--- template-parts/blog/blog-view.php
+++ template-parts/blog/blog-view.php
@@ -11,13 +11,15 @@ function debug_to_console($data, $context = 'Debug in Console') {
     echo $output;
 }
 $post_tags = get_the_tags($post->ID);
-debug_to_console($post_tags, 'post_tag');
+$post_date = date_create($post->post_date);
+$publishedOn = date_format($post_date, 'l, F Y');
+debug_to_console($post, 'post');
 ?>
 
 <div class="p-5 blog_view_page">
     <h2 class="text-center font-bold"> <? echo the_title();  ?> </h2>
     <div class="container-fluid d-flex mx-auto">
-        <div class="col-9">
+        <div class="col-8">
             <div class="blog_image_view">
                 <? echo the_post_thumbnail(); ?>
             </div>
@@ -25,17 +27,23 @@ debug_to_console($post_tags, 'post_tag');
                 <? echo the_content(); ?>
             </div>
         </div>
-        <div class="col-3">
-            <div class="tags gap-4">
-                <h6>Post Tags</h6>
-                <div class="post_tags align-items-center justify-content-between  d-flex">
-                    <? 
-                        foreach ($post_tags as $post_tag) { 
-                            get_template_part('template-parts/blog/post-tag', 'post-tag', array('post_tag' => $post_tag));
-                        }
-                    ?>
-                </div>
+        <div class="col-4 tags py-5">
+            <div class="author d-flex align-items-center">
+                <h6>Posted by: <? echo get_the_author(); ?></h6>
             </div>
+            <h6 class="posted_on">Date: <? echo $publishedOn; ?></h6>
+            <?if ($post_tags){ ?>
+                <div class="post_tag d-flex flex-wrap">
+                    <h6>Post Tags</h6>
+                    <div class="align-items-center flex-wrap justify-content-start gap-4 d-flex">
+                        <? 
+                            foreach ($post_tags as $post_tag) { 
+                                get_template_part('template-parts/blog/post-tag', 'post-tag', array('post_tag' => $post_tag));
+                            }
+                        ?>
+                    </div>
+                </div>
+            <? } ?>
         </div>
     </div>
 </div>
--- template-parts/blog/post-tag.php
+++ template-parts/blog/post-tag.php
@@ -1,5 +1,5 @@
 <? $post_tag = $args['post_tag'] ?>
 
-<div class="post-tag">
+<div class="post-tag my-1 mr-2">
     <? echo $post_tag->name; ?>
 </div>
