fe8b76957ce0c7333d15274df485df8107b3b1ea
BisiDavi <oludavidconnect@gmail.com>
2023-03-28 12:42:09 +0100

    lightbox asset added

--- template-parts/gallery/index.php
+++ template-parts/gallery/index.php
@@ -16,7 +16,7 @@
             while($galleryPost->have_posts()){
                 $galleryPost->the_post();
                 ?>
-                <a class="gallery-view-item" href="#" data-lightbox="example-set">
+                <a class="gallery-view-item" href="<?echo get_the_post_thumbnail_url($post->ID,'post_thumbnail'); ?>" data-lightbox="example-set">
                     <? echo the_post_thumbnail(); ?>
                 </a>
            <? }
