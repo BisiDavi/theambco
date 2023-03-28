be843916a229759efa0c364f102da7272e0b0f5e
BisiDavi <oludavidconnect@gmail.com>
2023-03-28 01:43:33 +0100

    styles updated

--- template-parts/contact/index.php
+++ template-parts/contact/index.php
@@ -17,8 +17,8 @@
     style="background:linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(251, 7, 7, 0.33)), url(<? echo $content_url;?>);
       background-size:cover;">
     </div>
-    <div class="col-lg-6 col-12 py-5 order-1 order-lg-2 h-100 right">
-        <h3 class="text-center mt-lg-5 mt-4">Require Our Services?</h3>
+    <div class="col-lg-6 col-12 py-5 px-4 px-lg-0 order-1 order-lg-2 h-100 right">
+        <h3 class="text-center mt-5">Require Our Services?</h3>
         <? echo $contact_post; ?>
         <? get_template_part('template-parts/contact/contact-form', 'contact-form'); ?>
     </div>
