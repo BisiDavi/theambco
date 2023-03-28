273f396383e1759b8ff72794ea5ba31915130429
BisiDavi <oludavidconnect@gmail.com>
2023-03-28 11:55:02 +0100

    gallery updated

---
+++ template-parts/gallery/index.php
@@ -0,0 +1,44 @@
+<? 
+     $galleryPostArgs = array(
+        'post_type' => 'post', 
+        'cat' => 62, 
+        'paged' => get_query_var('paged'),
+        'order' => 'ASC',
+        'orderby' => 'date',
+        'posts_per_page' => 12
+    );
+    $galleryPost = new WP_Query($galleryPostArgs);
+?>
+
+<div class="gallery-view container-fluid grid">
+    <?
+        if($galleryPost->have_posts()){
+            while($galleryPost->have_posts()){
+                $galleryPost->the_post();
+                ?>
+                <div class="gallery-view-item">
+                    <? echo the_post_thumbnail(); ?>
+                </div>
+           <? }
+        }
+    ?>
+</div>
+<div class="pagination">
+    <?php 
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $galleryPost->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( '<<', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( '>>', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
+    ?>
+</div>
