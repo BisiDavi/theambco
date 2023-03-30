70573f81a77e0de0e11fd550a1bc660d52fecc46
BisiDavi <oludavidconnect@gmail.com>
2023-03-30 12:05:55 +0100

    testimonial slider updated

---
+++ assets/js/testimonial-slider.js
@@ -0,0 +1,42 @@
+let blogSwiper = new Swiper(".testimonialSlider", {
+  slidesPerView: 8,
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".testimonialButtonNext",
+    prevEl: ".testimonialButtonPrev",
+  },
+  pagination: {
+    el: ".testimonialPagination",
+    type: "bullets",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    350: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    760: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    1440: {
+      slidesPerView: 8,
+      spaceBetween: 10,
+    },
+  },
+});
--- template-parts/home/testimonial-view.php
+++ template-parts/home/testimonial-view.php
@@ -20,14 +20,23 @@
 
 <div class="testimonial_view container-fluid py-5">
     <h4 class="text-center">Testimonials</h4>
-    <div class="d-flex align-items-start mt-4">
-        <?
-            if($testimonialPost->have_posts()){
-                while($testimonialPost->have_posts()){
-                    $testimonialPost->the_post();
-                    get_template_part('template-parts/home/testimonial','testimonial');
-                }
-            }
-        ?>
+    <div class="d-flex flex-column align-items-start mt-4 testimonial_view_group">
+        <div class="swiper testimonialSlider">
+            <div class="swiper-wrapper">
+                <?
+                    if($testimonialPost->have_posts()){
+                        while($testimonialPost->have_posts()){
+                            $testimonialPost->the_post();
+                            get_template_part('template-parts/home/testimonial','testimonial');
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+        <div class="paginate-slider">
+            <div class="swiper-pagination testimonialPagination"></div>
+        </div>
+        <div class="swiper-button-prev testimonialButtonPrev"></div>
+        <div class="swiper-button-next testimonialButtonNext"></div>
     </div>
 </div>
