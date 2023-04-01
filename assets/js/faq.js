f084311b2cd2aaedd368070976ef94f4c3dd91c4
BisiDavi <oludavidconnect@gmail.com>
2023-04-01 05:28:47 +0100

    faq updated

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -7,9 +7,11 @@ faqHeaders.forEach((element) => {
     if (!element.classList.contains("active")) {
       element.classList.add("active");
       element.classList.add("animate__fadeIn");
+      element.classList.remove("updated");
     } else {
       element.classList.remove("animate__fadeIn");
       element.classList.remove("active");
+      element.classList.add("updated");
     }
   });
 });
