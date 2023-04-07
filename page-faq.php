13bc8e8a38377dbf771214bbae6e71532c77404d
BisiDavi <oludavidconnect@gmail.com>
2023-04-07 01:59:52 +0100

    faq updated

---
+++ page-faq.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * @package Wordpress
+ * @subpackage FAQ
+ * @since TheAmbulanceCompany 1.0
+ */
+
+get_header('home');
+
+get_template_part('template-parts/contact/faqs', 'faqs');
+
+get_footer('home');
+?>
