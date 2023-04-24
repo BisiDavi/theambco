e10f0075ce2403fc3964bbe414d2de58e7905cb2
BisiDavi <oludavidconnect@gmail.com>
2023-04-24 15:57:14 +0100

    color plugin added

---
+++ plugin/color_plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  The Ambulance Color Plugin
+Plugin URI:   https://www.theambulancecompany.com
+Description:  This plugin allows you change the color of the site
+Version:      1.0
+Author:       Olubisi David 
+Author URI:   https://olubisi-david.vercel.app/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  Color Plugin
+Domain Path:  /languages
+*/
