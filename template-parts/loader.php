bb9f87b693b85a8fe8a1f17ad4b9419437a84431
BisiDavi <oludavidconnect@gmail.com>
2023-04-25 12:04:44 +0100

    top-banner updated

--- template-parts/loader.php
+++ template-parts/loader.php
@@ -1,3 +1,3 @@
-<div id="loaderAnimation" class="loader_animation animate__animated animate__fadeInLeft">
+<div id="loaderAnimation" class="loader_animation animate__animated animate__fadeInLeft" style="background-color:#9FCAC5;">
     <img src="<? echo get_template_directory_uri(); ?>/assets/images/ambulance.gif" />
 </div>
