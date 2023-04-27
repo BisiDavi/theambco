3a3b03b92a3e81315d474c09e1f3c2df986975e4
BisiDavi <oludavidconnect@gmail.com>
2023-04-27 13:58:35 +0100

    newsletter updated

--- template-parts/home/statistics-view.php
+++ template-parts/home/statistics-view.php
@@ -5,23 +5,25 @@
     $ambulances = get_post_field('ambulances', $post->ID);
 ?>
 
-<div class="statistics container mx-auto">
-    <div class="col-12 d-flex text-align-center">
-        <div class="col-md-3 col-6 count" data-default="0" data-value="<? echo $calls_received ?> ">
-           <h4 id="statsCount1"><? echo $calls_received ?></h4> 
-            <h6>Calls Received</h6>
-        </div>
-        <div class="col-md-3 col-6 count"  data-default="0"  data-value="<? echo $live_saved ?>">
-            <h4 id="statsCount2"><? echo $live_saved ?></h4>    
-            <h6>Lives Saved</h6>
-        </div>
-        <div class="col-md-3 col-6 count"  data-default="0"  data-value="<? echo $avg_resp_time; ?>">
-            <h4 id="statsCount3"><? echo $avg_resp_time; ?></h4>   
-            <h6>Average Response Time</h6>
-        </div>
-        <div class="col-md-3 col-6 count"  data-default="0"  data-value="<? echo $ambulances; ?>">
-            <h4 id="statsCount4"><? echo $ambulances; ?></h4>   
-            <h6>Ambulances</h6>
+<div class="statistics_wrapper bg-white">
+    <div class="statistics container mx-auto">
+        <div class="col-12 d-flex text-align-center">
+            <div class="col-md-3 col-6 count" data-default="0" data-value="<? echo $calls_received ?> ">
+            <h4 id="statsCount1"><? echo $calls_received ?></h4> 
+                <h6>Calls Received</h6>
+            </div>
+            <div class="col-md-3 col-6 count"  data-default="0"  data-value="<? echo $live_saved ?>">
+                <h4 id="statsCount2"><? echo $live_saved ?></h4>    
+                <h6>Lives Saved</h6>
+            </div>
+            <div class="col-md-3 col-6 count"  data-default="0"  data-value="<? echo $avg_resp_time; ?>">
+                <h4 id="statsCount3"><? echo $avg_resp_time; ?></h4>   
+                <h6>Average Response Time</h6>
+            </div>
+            <div class="col-md-3 col-6 count"  data-default="0"  data-value="<? echo $ambulances; ?>">
+                <h4 id="statsCount4"><? echo $ambulances; ?></h4>   
+                <h6>Ambulances</h6>
+            </div>
         </div>
     </div>
 </div>
